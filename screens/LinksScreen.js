05be0032b06d947c6ddb25b707dabf54828ec854
Allan Yeznaian <allanyeznaian@gmail.com>
2018-12-11 17:15:22 -0800

    compiling

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -6,16 +6,13 @@ import { TextInput } from "react-native-gesture-handler";
 export default class LinksScreen extends React.Component {
   constructor(props) {
     super(props);
-    // this.handleInputChange = this.handleInputChange.bind(this);
     this.state = {
       isShowingText: true,
+      showLyr: false,
       songObject: {},
-      songName: "",
       songsArr: [],
       songLyrics: ""
     };
-
-    // Toggle the state every second
     setInterval(
       () =>
         this.setState(previousState => ({
@@ -24,58 +21,38 @@ export default class LinksScreen extends React.Component {
       1000
     );
   }
-
-  handleInputChange(event = {}) {
-    const name = event.target && event.target.name;
-    const value = event.target && event.target.value;
-
-    this.setState({ [name]: value });
-  }
-
   static navigationOptions = {
     title: "Links"
   };
 
-  submit = () => {
-    const songName = this.state.songName;
-    const lyrics = this.state.songLyrics;
+  next = () => {
+    this.setState({ showLyr: true });
+  };
 
-    this.state.songsArr.push("song: " + songName + "," + lyrics);
+  submit = () => {
+    const songName = this.state.tit;
+    // const lyrics = this.state.lyr;
+    // this.state.songsArr.push('song: ' + songName + ' ,lyrics:' + lyrics);
+    // alert(this.state.songsArr);
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-         * content, we just wanted to provide you with some helpful links */}
-        {/* <ExpoLinksView /> */}
-        <Text>Song:</Text>
-        <TextInput
-          value={this.state.songName}
-          onChange={e => {
-            this.setState({ songName: e.target.value });
-          }}
-        />
-        <Text>lyrics:</Text>
-        <TextInput
-          name="songLyrics"
-          value={this.state.songLyrics}
-          onChange={e => {
-            this.setState({ songLyrics: e.target.value });
-          }}
-        />
-        <Button title="submit" onPress={() => this.submit()}>
-          submit
-        </Button>
-
         <Text>
-          {this.state.songsArr.map(song => {
-            <Text key={song.songName}>
-              <Button>{song.songName}</Button>
-              <Text>{song.lyrics}</Text>
-            </Text>;
-          })}
+          {!this.state.showLyr ? "Enter your new songs name below:" : ""}
         </Text>
+        <TextInput
+          value={this.state.tit ? this.state.tit : ""}
+          onChangeText={tit => {
+            this.setState({ tit });
+          }}
+        />
+        <Button
+          title={!this.state.showLyr ? "next" : "update"}
+          onPress={this.next}
+        />
+        <Text>{this.state.tit}</Text>
       </ScrollView>
     );
   }
